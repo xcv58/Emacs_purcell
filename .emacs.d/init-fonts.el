25e08b5aa10cb1564f3d0d5db71a9f40091be402
xcv58 <chenyihonglove@gmail.com>
2013-11-11 01:03:53 -0500

    change font increase ratio to default

--- .emacs.d/init-fonts.el
+++ .emacs.d/init-fonts.el
@@ -41,11 +41,11 @@ by the :height face attribute."
 
 (defun sanityinc/increase-default-font-height ()
   (interactive)
-  (sanityinc/increment-default-font-height 20))
+  (sanityinc/increment-default-font-height 10))
 
 (defun sanityinc/decrease-default-font-height ()
   (interactive)
-  (sanityinc/increment-default-font-height -20))
+  (sanityinc/increment-default-font-height -10))
 
 (global-set-key (kbd "C-M-=") 'sanityinc/increase-default-font-height)
 (global-set-key (kbd "C-M--") 'sanityinc/decrease-default-font-height)
